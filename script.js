1efb2702d992a76daaf71a6c3de2cf8fdabe403d
JEYASRI S <108861190+jeyasri-senthil@users.noreply.github.com>
2024-02-22 20:57:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let inputBox = document.querySelector('#inputBox');
+    let list = document.querySelector('#list');
+
+    inputBox.addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            addItem(this.value)
+            this.value = ""
+        }
+    })
+
+    let addItem = (inputBox) => {
+        let listItem = document.createElement("li");
+        listItem.innerHTML = `${inputBox}<i></i>`;
+        listItem.addEventListener("click", function () {
+            this.classList.toggle('done');
+        })
+        listItem.querySelector("i").addEventListener("click", function () {
+            listItem.remove();
+        })
+        list.appendChild(listItem);
+    }
+});
